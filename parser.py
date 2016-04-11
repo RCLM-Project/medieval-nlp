43bc0ac0c313dcf641480ed6e7c4ee61ac806ce7
Jacob Bieker <jacob@bieker.tech>
2016-04-11 13:14:44 -0700

    Start analyzing sources

--- parser.py
+++ parser.py
@@ -1,3 +1,12 @@
 __author__ = 'Jacob Bieker'
 import os
-import nltk
+import nltk, re, pprint
+from nltk import word_tokenize
+nltk.download()
+
+source = open(os.path.join("primary-sources", "annalsofrogerdeh01hoveuoft_djvu.txt"))
+raw = source.read()
+
+# NLTK testing
+tokens = word_tokenize(raw)
+tagged = nltk.pos_tag(raw)

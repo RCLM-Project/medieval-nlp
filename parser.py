bec172519d6ba514e538acbe49f1c09f27cdd031
Jacob Bieker <jacob@bieker.tech>
2016-04-11 17:20:54 -0700

    Add preprocessing step
    Given by NTLK examples

--- parser.py
+++ parser.py
@@ -2,11 +2,24 @@ __author__ = 'Jacob Bieker'
 import os
 import nltk, re, pprint
 from nltk import word_tokenize
-nltk.download()
+#nltk.download()
 
-source = open(os.path.join("primary-sources", "annalsofrogerdeh01hoveuoft_djvu.txt"))
-raw = source.read()
+
+def ie_preprocess(document):
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
+
+annals_of_roger = open(os.path.join("primary-sources", "annalsofrogerdeh01hoveuoft_djvu.txt"))
+raw_rogers = annals_of_roger.read()
+
+itinerary_through_ireland = open(os.path.join("primary-sources", "itinerarythroug00girauoft_djvu.txt"))
+raw_ireland = itinerary_through_ireland.read()
 
 # NLTK testing
-tokens = word_tokenize(raw)
-tagged = nltk.pos_tag(raw)
+processed_roger = ie_preprocess(raw_rogers)
+processed_ireland = ie_preprocess(raw_ireland)
+
+
+

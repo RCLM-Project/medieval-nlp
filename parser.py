bb0002b596bd072d67d9b8191bcf3104d1c1961b
Jacob Bieker <jacob@bieker.tech>
2016-05-19 22:12:19 -0700

    Generate word cloud images

--- parser.py
+++ parser.py
@@ -5,7 +5,9 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from multiprocessing import Pool
 #nltk.download()
-
+from wordcloud import WordCloud
+import numpy as np
+from PIL import Image
 
 def ie_preprocess(document):
     sentences = nltk.sent_tokenize(document)
@@ -35,12 +37,23 @@ def counting_words(word_list, tagged_text, nltk_text, nltk_processed):
         word_count = 0
         for word in small_list:
             word_count += nltk_text.lower().count(word) / len(tagged_text)
-            print("Word: " + word)
-            print(nltk_processed.concordance(word))
-            print("Similar Words:")
-            print(nltk_processed.similar(word))
+            #print("Word: " + word)
+            #print(nltk_processed.concordance(word))
+            #print("Similar Words:")
+            #print(nltk_processed.similar(word))
         print("Similar Words To: " + str(small_list[0]))
         print("Ratio to full text: " + str(word_count * 100))
+
+
+def wordcloud_make(text, discipline, image_name):
+    alice_mask = np.array(Image.open(os.path.join(image_name)))
+    wc = WordCloud(background_color="white", mask=alice_mask)
+
+    # generate word cloud
+    wc.generate(text)
+
+    # store to file
+    wc.to_file(os.path.join(str(discipline + ".png")))
 
 dict_of_texts = {}
 dict_of_freq = {}
@@ -53,6 +66,27 @@ with open("outfile.txt", "w") as output:
         with open(os.path.join("primary-sources", filename)) as nltk_text:
             print(filename)
             raw_data = nltk_text.read()
+            mod_raw = []
+            for character in raw_data:
+                if character.isalnum() or character == " " or character == "\n" or character == "-":
+                    mod_raw.append(character)
+
+            new_raw = ""
+            new_raw = new_raw.join(mod_raw)
+            if filename == "William of Malmesbury's Chronicle of the Kings of England.txt":
+                wordcloud_make(new_raw, "malmesbury", "kings-of-england-pic.png")
+            elif filename == "Master Wace's Chronicle of the Norman Conquest.txt":
+                wordcloud_make(new_raw, "wace", "wace-pic.png")
+            elif filename == "Full The ecclesastical history of England.txt":
+                wordcloud_make(new_raw, "vitalis", "orderic-vitalis-pic.png")
+            elif filename == "Full The annals of Roger de Hoveden.txt":
+                wordcloud_make(new_raw, "roger", "roger-pic.png")
+            elif filename == "Florence of Worcester's Chroncile.txt":
+                wordcloud_make(new_raw, "florence", "john-pic.png")
+            elif filename == "Chronicle of Henry Huntingdon.txt":
+                wordcloud_make(new_raw, "henry", "huntingdon-pic.png")
+            elif filename == "Anglo-Saxon Chronicle.txt":
+                wordcloud_make(new_raw, "saxon", "anglo-saxon-pic.png")
             nltk_processed = nltk.Text(raw_data)
             print(nltk_processed.collocations())
             # After removing stopwords and punctuation do analysis again
@@ -82,7 +116,14 @@ with open("outfile.txt", "w") as output:
                                  ["title", "earl", "king", "earls", "kings", "duke", "dukes", "regent", "regents",
                                   "knight", "knights", "noble", "nobles", "lord", "lords"]]
             counting_words(interesting_words, processed, raw_data, nltk_processed)
+            # N-grams
+            ngrams = nltk.ngrams(processed, n=5)
+            fdist = nltk.FreqDist(ngrams)
+            keys = fdist.keys()
+            print("Ngrams")
+            print(fdist.most_common(20))
             print("\n\n")
+
 
     output.write("Total words: " + str(total_words))
     output.write("Total Sentences: " + str(total_sentences))
